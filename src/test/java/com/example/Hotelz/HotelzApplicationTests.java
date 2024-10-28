17664cb647cbcee41aba122b1d4f11d0c777c00b
Anderson Zango <azango94@gmail.com>
2024-10-28 13:06:10 +0200

    First Commmit

---
+++ src/test/java/com/example/Hotelz/HotelzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hotelz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
